a23406d6f2fbd7f1538e7709b6e27926ea83e198
nxzott <154569410+nxzott@users.noreply.github.com>
2025-04-28 10:03:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // KONFIGURASI: Ganti dengan repo dan folder kamu!
 const GITHUB_OWNER = "nxzott";
 const GITHUB_REPO = "addons";
-const GITHUB_PATH = "files"; // kosongkan "" jika root repo
+const GITHUB_PATH = "file"; // kosongkan "" jika root repo
 
 let addons = [
     // Tambahkan manual jika ingin
